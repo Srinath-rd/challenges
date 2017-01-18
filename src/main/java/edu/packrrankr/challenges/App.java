9a6c7edf738ff6bd39bb81d4f59d8b9a3e19cb56
Srinath Pullaiahgari <srinath.pullaiahgari@wpsic.com>
2017-01-18 12:05:36 -0600

    initial commit

---
+++ src/main/java/edu/packrrankr/challenges/App.java
@@ -0,0 +1,13 @@
+package edu.packrrankr.challenges;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
